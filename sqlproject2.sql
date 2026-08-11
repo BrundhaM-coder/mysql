a0330032a70327a5dddd67686e4deab84942c786
Brundha M <brundham29@gmail.com>
2026-08-11 18:08:26 +0530

    Create customer table in education database
    Added SQL commands to create a customer table and retrieve its details.

--- sqlproject2.sql
+++ sqlproject2.sql
@@ -4,4 +4,16 @@ show databases;
 use education;
 drop database test2;
 
+create table customer(
+  customerid int(10),
+  customer_name varchar(20),
+  customer_address varchar(90)
+  );
+
+
+select* from customer;
+
+desc customer;
+
+
 
